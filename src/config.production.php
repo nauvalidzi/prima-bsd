713796336a4df0e8bf445da279231cd08dce00a5
unknown <nauval.hafidzi@gmail.com>
2021-06-16 17:48:29 +0700

    update progress

--- src/config.production.php
+++ src/config.production.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "H62jYBwfS32xFyVt", // API Secret Key
+        "SECRET_KEY" => "1lKlAn3y2mgmRP3J", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
