ab8a20cc0886b975e441930f1c76c48b10c7f32c
unknown <nauval.hafidzi@gmail.com>
2021-11-04 19:55:03 +0700

    bugfix Penagihan Customer

--- views/PenagihanCustomer.php
+++ views/PenagihanCustomer.php
@@ -12,56 +12,55 @@ $PenagihanCustomer = &$Page;
 
 		switch (strtoupper($_GET['umur']) ?? '') {
 			case 'H-2':
-				$umur = "AND H-2";
+				$umur = "HAVING umur_faktur < -1 AND umur_faktur > -3"; // H-2
 				break;
 			case 'H+1':
-				$umur = "AND H+1";
+				$umur = "HAVING umur_faktur < 2 AND umur_faktur > 0"; // H+1
 				break;
 			case 'H+7':
-				$umur = "AND H+7";
+				$umur = "HAVING umur_faktur < 8 AND umur_faktur > 5"; // H+7
 				break;
 			case 'H+14':
-				$umur = "AND H+14";
+				$umur = "HAVING umur_faktur < 15 AND umur_faktur > 13"; // H+14
 				break;
 			case 'H+21':
-				$umur = "AND H+21";
+				$umur = "HAVING umur_faktur < 22 AND umur_faktur > 20"; // H+21
 				break;
 			case 'H+28':
-				$umur = "AND H+28";
+				$umur = "HAVING umur_faktur < 29 AND umur_faktur > 27"; // H+28
 				break;
 			case 'H+30':
-				$umur = "AND H+30";
+				$umur = "HAVING umur_faktur < 31 AND umur_faktur > 29"; // H+30
 				break;
             default:
             	$umur = null;
             	break;
         }
 		
-		$query = "SELECT o.id as idorder,
-					date_format(o.tanggal, '%Y-%m-%d') AS tgl_order,
+		$query = "SELECT o.id AS idorder, DATE_FORMAT(o.tanggal, '%Y-%m-%d') AS tgl_order,
 					o.kode AS kode_order,
 					c.nama AS nama_customer,
-					c.hp AS nomor_handphone,
-					SUM(od.total) AS nilai_po,
-					MIN(DATE_FORMAT(i.tglinvoice, '%Y-%m-%d')) AS tgl_faktur, 
-					CASE
-						WHEN SUM(i.totaltagihan) IS NULL THEN SUM(od.total) 
-						ELSE SUM(i.totaltagihan)
-					END AS nilai_faktur, 
-					CASE 
-						WHEN SUM(i.sisabayar) IS NULL THEN SUM(od.total) 
-						ELSE SUM(i.sisabayar) 
-					END AS piutang,
-					TIMESTAMPDIFF(DAY, MIN(i.tglinvoice), NOW()) as umur_faktur,
-					IFNULL(DATE_FORMAT(MAX(pn.tgl_antrian), '%Y-%m-%d %H:%i:%s'),'') as tgl_penagihan
-				  FROM `order` o
-				  JOIN order_detail od ON od.idorder = o.id
-				  JOIN customer c ON c.id = o.idcustomer
-				  LEFT JOIN invoice i ON i.idorder = o.id
-				  LEFT JOIN penagihan pn on o.kode = pn.kode_order
-				  WHERE i.sisabayar > 0 AND i.tglinvoice <= '{$jatuhtempo}'
-				  GROUP BY o.id, c.id";
-
+					c.hp AS nomor_handphone, SUM(od.total) AS nilai_po, 
+					CASE WHEN i.kode IS NULL THEN '-'
+					ELSE i.kode END AS kode_faktur, 
+					DATE_FORMAT(i.tglinvoice, '%Y-%m-%d') AS tgl_faktur, 
+					DATE_FORMAT(i.tglinvoice + INTERVAL `t`.`value` DAY, '%Y-%m-%d') AS jatuhtempo, 
+					TIMESTAMPDIFF(DAY, i.tglinvoice + INTERVAL `t`.`value` DAY, '{$jatuhtempo}') as umur_faktur,
+					`t`.`value` AS term_payment, 
+					SUM(i.totaltagihan) AS nilai_faktur, 
+					SUM(i.sisabayar) AS piutang, 
+					IFNULL(DATE_FORMAT(MAX(pn.tgl_antrian), '%Y-%m-%d %H:%i:%s'),'') AS tgl_penagihan
+				FROM `order` o
+				JOIN order_detail od ON od.idorder = o.id
+				JOIN customer c ON c.id = o.idcustomer
+				LEFT JOIN invoice i ON i.idorder = o.id
+				LEFT JOIN penagihan pn ON o.kode = pn.kode_order
+				LEFT JOIN termpayment t ON i.idtermpayment = t.id
+				WHERE i.sisabayar > 0 
+				GROUP BY o.id, c.id, i.id
+				{$umur}
+				ORDER BY jatuhtempo ASC";
+// print_r($query); die;
 		$result = ExecuteQuery($query)->fetchAll();
 	}
 ?>
@@ -74,6 +73,7 @@ $PenagihanCustomer = &$Page;
 					<li class="d-inline-block">
 						<label class="d-block">Umur Faktur</label>
 						<select name="umur" class="form-control" style="width: 10em;">
+							<option value="all">-- All --</option>
 							<option value="h-2" <?php echo isset($_GET['umur']) == 'h-2' ? 'selected' : null ?>>H-2</option>
 							<option value="h+1" <?php echo isset($_GET['umur']) == 'h+1' ? 'selected' : null ?>>H+1</option>
 							<option value="h+72" <?php echo isset($_GET['umur']) == 'h+72' ? 'selected' : null ?>>H+7</option>
