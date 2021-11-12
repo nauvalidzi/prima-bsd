5490a4595c0f8dcce318cc463dce15b88d3c1df8
unknown <nauval.hafidzi@gmail.com>
2021-11-12 16:00:54 +0700

    laporan invoice -> tambah field total bayar & improvement code

--- views/LaporanInvoice.php
+++ views/LaporanInvoice.php
@@ -6,72 +6,83 @@ namespace PHPMaker2021\distributor;
 $LaporanInvoice = &$Page;
 ?>
 <?php 
-	$status_selected = "all";
-	$status = null;
-
-	$payment = "all";
 	$listpayment = ExecuteQuery("SELECT * FROM tipepayment ORDER BY id ASC")->fetchAll();
 
 	$dateFrom = date('Y-m-01');
 	$dateTo = date('Y-m-t');
 
-	if(isset($_POST['srhDate'])){
-		$dateFrom = date('Y-m-d', strtotime($_POST['dateFrom']));
-		$dateTo = date('Y-m-d', strtotime($_POST['dateTo']));
+	if (isset($_GET['option'], $_GET['status'], $_GET['dateFrom'], $_GET['dateTo'])) {
+		$dateFrom = date('Y-m-d', strtotime($_GET['dateFrom']));
+		$dateTo = date('Y-m-d', strtotime($_GET['dateTo']));
 
-		if ($_POST['status'] == 'lunas') {
-			$status = " AND invoice.sisabayar < 1";
-			$status_selected = "lunas";
+		$status = null;
+		if ($_GET['status'] == 'lunas') {
+			$status = " AND i.sisabayar < 1";
 		}
 
-		if ($_POST['status'] == 'belumlunas') {
-			$status = " AND invoice.sisabayar > 0";
-			$status_selected = "belumlunas";
+		if ($_GET['status'] == 'belum-lunas') {
+			$status = " AND i.sisabayar > 0";
 		}
 
 		$tipepayment = null;
-		if ($_POST['payment'] != "all") {
-			$tipepayment = " AND idtipepayment = ".$_POST['payment'];
-			$payment_selected = ExecuteRow("SELECT payment FROM tipepayment WHERE id = {$_POST['payment']}");
+		if ($_GET['option'] != "all") {
+			$payment_id = ExecuteRow("SELECT id FROM tipepayment WHERE payment like '{$_GET['option']}'")['id'];
+			$tipepayment = " AND idtipepayment = {$payment_id}";
 		}
 
-		$query = "SELECT tglinvoice, invoice.kode AS kode_invoice, `order`.kode AS kode_po, 
-					customer.nama as nama_customer, totaltagihan, sisabayar, invoice.aktif, tipepayment.payment
-				  FROM invoice
-				  JOIN customer ON customer.id = invoice.idcustomer
-				  JOIN `order` ON `order`.id = invoice.idorder
-				  JOIN tipepayment ON tipepayment.id = invoice.idtipepayment
-				  WHERE tglinvoice BETWEEN '{$dateFrom}' AND '{$dateTo}' {$tipepayment} {$status}
-				  ORDER BY invoice.id ASC";
+		$query = "SELECT i.tglinvoice, i.kode AS kode_invoice, o.kode AS kode_po, 
+					c.nama as nama_customer, i.totaltagihan, sisabayar as piutang, 
+					t.payment, IFNULL(SUM(p.jumlahbayar),0) AS totalbayar
+				  FROM invoice i
+				  JOIN customer c ON c.id = i.idcustomer
+				  JOIN `order` o ON o.id = i.idorder
+				  JOIN tipepayment t ON t.id = i.idtipepayment
+				  LEFT JOIN pembayaran p ON p.idinvoice = i.id
+				  WHERE i.tglinvoice BETWEEN '{$dateFrom}' AND '{$dateTo}' {$tipepayment} {$status}
+				  GROUP BY i.id
+				  ORDER BY i.id ASC";
 
 		$result = ExecuteQuery($query)->fetchAll();
 	}
 ?>
+<style>
+	.col-flex {
+		width: 150px;
+		min-width: 150px;
+		max-width: 150px;
+	}
+	.col-description {
+		width: 250px;
+		min-width: 250px;
+		max-width: 250px;
+	}
+	.col-currency {
+		width: 175px;
+		min-width: 175px;
+		max-width: 175px;
+	}
+</style>
 <div class="container">
 	<div class="row">
-		<form method="post" action="<?php echo CurrentPage()->PageObjName ?>">
-			<?php if (Config("CHECK_TOKEN")) : ?>
-	            <input type="hidden" name="<?= $TokenNameKey ?>" value="<?= $TokenName ?>"><!-- CSRF token name -->
-	            <input type="hidden" name="<?= $TokenValueKey ?>" value="<?= $TokenValue ?>"><!-- CSRF token value -->
-            <?php endif; ?>
+		<form method="get" action="<?php echo CurrentPage()->PageObjName ?>">
 			<div class="col-md-12">
 				<ul class="list-unstyled">
 					<li class="d-inline-block">
 						<label class="d-block">Pembayaran:</label>
-						<select name="payment" class="form-control">
-							<option value="all" <?php echo $payment == "all" ? "selected" : "" ?>>-- All --</option>
+						<select name="option" class="form-control" style="width: 10em;">
+							<option value="all" <?php echo (isset($_GET['option']) && $_GET['option'] == "all") || empty($_GET['option']) ? "selected" : "" ?>>-- All --</option>
 							<?php foreach ($listpayment as $r) {
-								$selected = ($_POST['payment'] != "all") ? ($_POST['payment'] == $r['id']) ? "selected" : "" : "";
-								echo "<option value=".$r['id']." {$selected}>".$r['payment']."</option>";
+								$selected = isset($_GET['option']) && $_GET['option'] != "all" ? $_GET['option'] == strtolower($r['payment']) ? "selected" : "" : "";
+								echo "<option value=".strtolower($r['payment'])." {$selected}>{$r['payment']}</option>";
 							}?>
 						</select>
 					</li>
 					<li class="d-inline-block">
 						<label class="d-block">Status Invoice:</label>
-						<select name="status" class="form-control">
-							<option selected value="all" <?php echo ($status_selected == "all") ? "selected":""; ?>>-- All --</option>
-							<option value="lunas" <?php echo ($status_selected == "lunas") ? "selected":""; ?>>Lunas</option>
-							<option value="belumlunas" <?php echo ($status_selected == "belumlunas") ? "selected":""; ?>>Belum Lunas</option>
+						<select name="status" class="form-control" style="width: 10em;">
+							<option selected value="all" <?php echo isset($_GET['status']) && $_GET['status'] == "all" ? "selected":""; ?>>-- All --</option>
+							<option value="lunas" <?php echo isset($_GET['status']) && $_GET['status'] == "lunas" ? "selected":""; ?>>Lunas</option>
+							<option value="belum-lunas" <?php echo isset($_GET['status']) && $_GET['status'] == "belum-lunas" ? "selected":""; ?>>Belum Lunas</option>
 						</select>
 					</li>
 					<li class="d-inline-block">
@@ -83,9 +94,9 @@ $LaporanInvoice = &$Page;
 						<input type="date" class="form-control input-md" name="dateTo" value="<?php echo $dateTo ?>">
 					</li>
 					<li class="d-inline-block">
-						<button class="btn btn-primary btn-md p-2" type="submit" name="srhDate">Search <i class="fa fa-search h-3"></i></button>
+						<button class="btn btn-primary btn-md p-2" type="submit">Search <i class="fa fa-search h-3"></i></button>
 					</li>
-					<?php if(isset($_POST['srhDate'])) : ?>
+					<?php if (isset($_GET['option'], $_GET['status'], $_GET['dateFrom'], $_GET['dateTo'])) : ?>
 					<li class="d-inline-block">
 						<button type="button" class="btn btn-info btn-md p-2" onclick="exportTableToExcel('printTable')"><i class="mr-2 far fa-file-excel"></i>Export to Excel</button>
 					</li>
@@ -95,65 +106,71 @@ $LaporanInvoice = &$Page;
 		</form>
 	</div>
 	<div class="row">
-	    <?php if(isset($_POST['srhDate'])) : ?>
+	    <?php if (isset($_GET['option'], $_GET['status'], $_GET['dateFrom'], $_GET['dateTo'])) : ?>
 	    <table class="table ew-table table-bordered" id="printTable">
 	    	<thead>
 				<tr>
 					<th colspan="10" class="text-center">
 						<h4 class="my-2">Laporan Invoice</h4>
-						<p class="mt-3">Pembayaran: <?php echo ($_POST['payment'] == "all") ? "All" : $payment_selected['payment']; ?><br />Status Invoice: <?php echo ucwords($status_selected) ?><br />Periode: <?php echo tgl_indo($dateFrom) . ' - ' . tgl_indo($dateTo) ?></p>
+						<p class="mt-3">Pembayaran: <?php echo ucwords($_GET['option']); ?><br />Status Invoice: <?php echo ucwords($_GET['status']) ?><br />Periode: <?php echo tgl_indo($dateFrom) . ' - ' . tgl_indo($dateTo) ?></p>
 					</th>
 				</tr>
 	    		<tr>
 	    			<th class="text-center">No.</th>
-		    		<th class="text-center">Tgl Invoice</th>
-		    		<th class="text-center">Kode Invoice</th>
-		    		<th class="text-center">Kode P.O.</th>
-		    		<th class="text-center">Total Tagihan</th>
-		    		<th class="text-center">Sisa Bayar</th>
-		    		<?php if ($_POST['payment'] == "all"): ?>
-		    		<th class="text-center">Pembayaran</th>
+		    		<th class="text-center col-flex">Tgl Invoice</th>
+		    		<th class="text-center col-flex">Kode Invoice</th>
+		    		<th class="text-center col-flex">Kode Order</th>
+		    		<th class="text-center col-description">Nama Pelanggan</th>
+		    		<th class="text-center col-currency">Total Tagihan</th>
+		    		<th class="text-center col-currency">Piutang</th>
+		    		<th class="text-center col-currency">Total Bayar</th>
+		    		<?php if ($_GET['option'] == "all"): ?>
+		    		<th class="text-center col-flex">Pembayaran</th>
 			    	<?php endif; ?>
-		    		<?php if ($status_selected == "all"): ?>
-		    		<th class="text-center">Status</th>
+		    		<?php if ($_GET['status'] == "all"): ?>
+		    		<th class="text-center col-flex">Status</th>
 			    	<?php endif; ?>
 	    		</tr>
 	    	</thead>
 	    	<tbody>
     		<?php if (!empty($result)): ?>
-    			<?php $i = 1; $ext = ['totaltagihan' => 0, 'sisabayar' => 0]; ?>
+    			<?php $i = 1; $ext = ['totaltagihan' => 0, 'piutang' => 0, 'totalbayar' => 0]; ?>
 	    		<?php foreach($result as $row): ?>
 	    		<tr>
 	    			<td class="text-center"><?php echo $i; ?></td>
-		    		<td><?php echo tgl_indo($row['tglinvoice']) ?></td>
-		    		<td><?php echo $row['kode_invoice'] ?></td>
-		    		<td><?php echo $row['kode_po'] . ', ' . $row['nama_customer'] ?></td>
+		    		<td class="text-center"><?php echo tgl_indo($row['tglinvoice']) ?></td>
+		    		<td class="text-center"><?php echo $row['kode_invoice'] ?></td>
+		    		<td class="text-center"><?php echo $row['kode_po'] ?></td>
+		    		<td><?php echo $row['nama_customer'] ?></td>
 		    		<td>Rp. <span class="float-right"><?php echo number_format($row['totaltagihan'], 2, ",", ".") ?></span></td>
-		    		<td>Rp. <span class="float-right"><?php echo number_format($row['sisabayar'], 2, ",", ".") ?></span></td>
-		    		<?php if ($_POST['payment'] == "all"): ?>
+		    		<td>Rp. <span class="float-right"><?php echo number_format($row['piutang'], 2, ",", ".") ?></span></td>
+		    		<td>Rp. <span class="float-right"><?php echo number_format($row['totalbayar'], 2, ",", ".") ?></span></td>
+		    		<?php if ($_GET['option'] == "all"): ?>
 		    		<td class="text-center"><?php echo $row['payment'] ?></td>
 			    	<?php endif; ?>
-		    		<?php if ($status_selected == "all"): ?>
-		    		<td class="text-center"><?php echo $row['sisabayar'] < 1 ? 'Lunas' : 'Belum Lunas';  ?></td>
+		    		<?php if ($_GET['status'] == "all"): ?>
+		    		<td class="text-center"><?php echo $row['piutang'] < 1 ? 'Lunas' : 'Belum Lunas';  ?></td>
 			    	<?php endif; ?>
 	    		</tr>
 	    		<?php 
 	    			$ext['totaltagihan'] += $row['totaltagihan'];
-	    			$ext['sisabayar'] += $row['sisabayar'];
+	    			$ext['piutang'] += $row['piutang'];
+	    			$ext['totalbayar'] += $row['totalbayar'];
 	    		?>
 		    	<?php $i++; endforeach; ?>
 	    	<?php else: ?>
 	    		<tr>
-	    			<td colspan="8" class="text-center">Tidak ada data.</td>
+	    			<td colspan="10" class="text-center">Tidak ada data.</td>
 	    		</tr>
 	    	<?php endif; ?>
 	    	</tbody>
     		<?php if (!empty($result)): ?>
 	    	<tfoot>
 	    		<tr>
-	    			<th colspan="4" class="text-right">Grand Total :</th>
+	    			<th colspan="5" class="text-right">Grand Total :</th>
 	    			<th>Rp. <span class="float-right"><?php echo number_format($ext['totaltagihan'], 2, ",", ".") ?></span></th>
-	    			<th>Rp. <span class="float-right"><?php echo number_format($ext['sisabayar'], 2, ",", ".") ?></span></th>
+	    			<th>Rp. <span class="float-right"><?php echo number_format($ext['piutang'], 2, ",", ".") ?></span></th>
+	    			<th>Rp. <span class="float-right"><?php echo number_format($ext['totalbayar'], 2, ",", ".") ?></span></th>
 	    		</tr>
 	    	</tfoot>
 			<?php endif; ?>
@@ -194,5 +211,4 @@ $LaporanInvoice = &$Page;
 		<?php endif; ?>
 	</div>
 </div>
-
 <?= GetDebugMessage() ?>
